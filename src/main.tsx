9a94f432de045b8a16ff4d34c1659e0b239c9f4d
Antônio Prado <antonioeliaspr@gmail.com>
2026-03-07 00:30:00 -0300

    fix: update .htaccess options and enhance React DevTools integration

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ === 'object') {
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject = () => {}
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/[hash].js',
+        entryFileNames: 'assets/[hash].js',
+        assetFileNames: 'assets/[hash].[ext]',
+      },
+    },
+  },
 })
